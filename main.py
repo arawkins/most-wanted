2948acc076b7aab522b2e84f5fa7c14cde9f64fe
Alan Rawkins <arawkins@gmail.com>
2017-06-23 10:51:11 -0700

    Added placeholder data, basic get request to back end

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ def create():
         return "no data submitted"
 
 @app.route("/get/<client_name>")
-def get(client_name):
-    client_requests = [
+def get(client_name=''):
+    client_a_requests = [
         {
             "title":"Make Web App",
             "description": "Hopefully this doesn't take too long",
@@ -25,4 +25,34 @@ def get(client_name):
             "description": "Two are better than one (most of the time)",
         },
     ]
+
+    client_b_requests = [
+        {
+            "title":"Cook a sandwich",
+            "description": "Grilled Cheese if you please",
+        },
+        {
+            "title":"Brew Coffee",
+            "description": "Medium Roast, with cream",
+        },
+    ]
+
+    client_c_requests = [
+        {
+            "title":"Take over the world",
+            "description": "The same thing we do every night.",
+        },
+        {
+            "title":"Retrieve the carrots",
+            "description": "From the farmer's garden",
+        },
+    ]
+    client_requests = []
+    if client_name == 'client_a':
+        client_requests = client_a_requests
+    elif client_name == 'client_b':
+        client_requests = client_b_requests
+    elif client_name == 'client_c':
+        client_requests = client_c_requests
+
     return jsonify(client_requests)
